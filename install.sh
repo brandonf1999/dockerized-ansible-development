e769b53717954330ddee9cebde5826826bbfd0b9
Brandon Foos <brandon@brandonfoos.com>
2024-07-01 08:01:26 -0400

    minor changes

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 
-echo "docker build -t ansible-controller $(pwd)/" > /usr/local/sbin/ansible-playbook
-echo "docker run ansible-controller ansible-playbook" >> /usr/local/sbin/ansible-playbook
+echo "docker build -t ansible-controller:latest $(pwd)/" > /usr/local/sbin/ansible-playbook
+echo 'docker run ansible-controller ansible-playbook $@' >> /usr/local/sbin/ansible-playbook
 chmod 770 /usr/local/sbin/ansible-playbook
+
+echo "docker build -t ansible-controller:latest $(pwd)/" > /usr/local/sbin/ansible
+echo 'docker run ansible-controller ansible $@' >> /usr/local/sbin/ansible
+chmod 770 /usr/local/sbin/ansible
+
 
 
 
